d1be6cd0acd5c239ea7f9c58740a50a4f4c56892
ミック(Sirapat) <sirapat.jr@gmail.com>
2025-05-06 11:16:06 +0900

    add new config

--- eslint.config.js
+++ eslint.config.js
@@ -38,6 +38,7 @@ export default tseslint.config(
         "error",
         { checksVoidReturn: { attributes: false } },
       ],
+      "@typescript-eslint/prefer-nullish-coalescing": "error",
       "drizzle/enforce-delete-with-where": [
         "error",
         { drizzleObjectName: ["db", "ctx.db"] },
--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+
+    //Theo mentions this is a great idea
+    //bc we can add CI later on to check this
+    //but he's annoyed by the fact that deployment is blocked
+    //but I think it's safter to have this turned off
+    // eslint: {
+    //     ignoreDuringBuilds: true,
+    // },
+    // typescript: {
+    //     ignoreBuildErrors: true,
+    // }
+};
 
 export default config;
