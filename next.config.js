9827ea8495600593acf2feb28dac442211b422c0
ミック(Sirapat) <sirapat.jr@gmail.com>
2025-05-06 11:21:08 +0900

    CI

--- next.config.js
+++ next.config.js
@@ -6,17 +6,13 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-
-    //Theo mentions this is a great idea
-    //bc we can add CI later on to check this
-    //but he's annoyed by the fact that deployment is blocked
-    //but I think it's safter to have this turned off
-    // eslint: {
-    //     ignoreDuringBuilds: true,
-    // },
-    // typescript: {
-    //     ignoreBuildErrors: true,
-    // }
+    // Already handled with CI so we can skip it
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    }
 };
 
 export default config;
